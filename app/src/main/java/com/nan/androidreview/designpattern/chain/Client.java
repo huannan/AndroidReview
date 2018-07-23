c3d70e9129ed4e8b90dcd3d6247703987381f6ad
吴焕楠 <wuhuannan@meizu.com>
2018-07-23 09:23:44 +0800

    [100]责任链模式3

--- app/src/main/java/com/nan/androidreview/designpattern/chain/Client.java
+++ app/src/main/java/com/nan/androidreview/designpattern/chain/Client.java
@@ -7,15 +7,19 @@ public class Client {
         Handler handler2 = new ConcreteHandler2();
         Handler handler3 = new ConcreteHandler3();
 
+        //动态生成处理者的责任链
         handler1.setNextHandler(handler2);
         handler2.setNextHandler(handler3);
 
+        //发送请求A
         Response response1 = handler1.handleRequest(new Request(1, "请求A"));
         System.out.println(response1.getReslust());
 
+        //发送请求B
         Response response2 = handler1.handleRequest(new Request(2, "请求B"));
         System.out.println(response2.getReslust());
 
+        //发送请求C
         Response response3 = handler1.handleRequest(new Request(3, "请求C"));
         System.out.println(response3.getReslust());
     }
